3fa3f3131e6c52d547ce78d65922dbc163afce82
Tianhao Zhou <tianhaoz@umich.edu>
2020-02-25 14:23:53 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const exec = require('@actions/exec');
+
+function updateFlutterWorkspace() async {
+  await exec.exec('flutter pub upgrade');
+}
 
 try {
   const flutterProjectWorkspace = core.getInput('flutter-project');
