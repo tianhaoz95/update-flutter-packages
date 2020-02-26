5f1e697c73a0c7807cf40e76f606112c0105d788
Tianhao Zhou <tianhaoz@umich.edu>
2020-02-25 22:34:26 -0800

    dev: add update function (#5)
    This PR adds the function to open a new branch and send out a pull request.

--- index.js
+++ index.js
@@ -1,11 +1,83 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const core = require("@actions/core");
+const exec = require("@actions/exec");
+const initContext = require("./lib/init");
 
-try {
-  const flutterProjectWorkspace = core.getInput('flutter-project');
-  console.log(`Analyzing Flutter project @ ${flutterProjectWorkspace}...`);
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
+async function updateFlutterWorkspace(context) {
+  let infoOutput = "";
+  let errorOuput = "";
+  await exec.exec(`git checkout -b ${context.tempBranch}`);
+  await exec.exec("flutter", ["pub", "upgrade"], {
+    cwd: context.flutterProjectWorkspace
+  });
+  await exec.exec("git", [
+    "config",
+    "--global",
+    "user.email",
+    context.gitEmail
+  ]);
+  await exec.exec("git", ["config", "--global", "user.name", context.gitUser]);
+  await exec.exec("git add -A");
+  await exec.exec('git commit -m "chore(flutterbot): update flutter packages"');
+  await exec.exec(
+    "git",
+    ["push", "-f", `https://github.com/${context.repo}.git`, context.tempBranch],
+    {
+      stdout: data => {
+        infoOutput += data.toString();
+      },
+      stderr: data => {
+        errorOuput += data.toString();
+      }
+    }
+  );
+  console.log(infoOutput);
+  console.log(errorOuput);
 }
+
+async function shouldOpenPullRequest(context) {
+  const pullRequestList = await context.octokit.pulls.list({
+    owner: context.username,
+    repo: context.project
+  });
+  for (const pullRequest of pullRequestList.data) {
+    if (
+      pullRequest.title === context.pullRequestTitle &&
+      pullRequest.state === 'open'
+    ) {
+      return false;
+    }
+  }
+  return true;
+}
+
+async function openUpdatePullRequest(context) {
+  await context.octokit.pulls.create({
+    owner: context.username,
+    repo: context.project,
+    title: context.pullRequestTitle,
+    head: context.tempBranch,
+    base: context.targetBranch
+  });
+}
+
+async function maybeOpenPullRequest(context) {
+  const openPullRequest = await shouldOpenPullRequest(context);
+  if (openPullRequest) {
+    console.log("not opened, open pull request");
+    await openUpdatePullRequest(context);
+  } else {
+    console.log("pull request already there, skip");
+  }
+}
+
+async function main() {
+  try {
+    const context = initContext();
+    await updateFlutterWorkspace(context);
+    await maybeOpenPullRequest(context);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+main();
