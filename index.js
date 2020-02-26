ddb5b4f8dd8fc0b80f3efd88cb71ae9eff121503
tianhaoz95 <tianhaoz@umich.edu>
2020-02-25 19:42:53 -0800

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ async function updateFlutterWorkspace(workspace, branch) {
   await exec.exec('git', ['config', '--global', 'user.name', 'Tianhao Zhou']);
   await exec.exec('git add -A');
   await exec.exec('git commit -m \"chore(flutterbot): update flutter packages\"');
-  await exec.exec('git', ['push' , `https://github.com/${process.env.GITHUB_REPOSITORY}.git`, branch], {
+  await exec.exec('git', ['push', '-f' , `https://github.com/${process.env.GITHUB_REPOSITORY}.git`, branch], {
     stdout: (data) => {
       infoOutput += data.toString();
     },
