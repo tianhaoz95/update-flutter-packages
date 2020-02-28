aae6e91c91039eebb462e0a2be2b9c5a28b52294
Tianhao Zhou <tianhaoz@umich.edu>
2020-02-27 21:20:45 -0800

    refactor: reorg code (#10)

--- index.js
+++ index.js
@@ -1,74 +1,7 @@
 const core = require("@actions/core");
-const exec = require("@actions/exec");
 const initContext = require("./lib/init");
-
-async function updateFlutterWorkspace(context) {
-  let infoOutput = "";
-  let errorOuput = "";
-  await exec.exec(`git checkout -b ${context.tempBranch}`);
-  await exec.exec("flutter", ["pub", "upgrade"], {
-    cwd: context.flutterProjectWorkspace
-  });
-  await exec.exec("git", [
-    "config",
-    "--global",
-    "user.email",
-    context.gitEmail
-  ]);
-  await exec.exec("git", ["config", "--global", "user.name", context.gitUser]);
-  await exec.exec("git add -A");
-  await exec.exec('git commit -m "chore(flutterbot): update flutter packages"');
-  await exec.exec(
-    "git",
-    ["push", "-f", `https://github.com/${context.repo}.git`, context.tempBranch],
-    {
-      stdout: data => {
-        infoOutput += data.toString();
-      },
-      stderr: data => {
-        errorOuput += data.toString();
-      }
-    }
-  );
-  console.log(infoOutput);
-  console.log(errorOuput);
-}
-
-async function shouldOpenPullRequest(context) {
-  const pullRequestList = await context.octokit.pulls.list({
-    owner: context.username,
-    repo: context.project
-  });
-  for (const pullRequest of pullRequestList.data) {
-    if (
-      pullRequest.title === context.pullRequestTitle &&
-      pullRequest.state === 'open'
-    ) {
-      return false;
-    }
-  }
-  return true;
-}
-
-async function openUpdatePullRequest(context) {
-  await context.octokit.pulls.create({
-    owner: context.username,
-    repo: context.project,
-    title: context.pullRequestTitle,
-    head: context.tempBranch,
-    base: context.targetBranch
-  });
-}
-
-async function maybeOpenPullRequest(context) {
-  const openPullRequest = await shouldOpenPullRequest(context);
-  if (openPullRequest) {
-    console.log("not opened, open pull request");
-    await openUpdatePullRequest(context);
-  } else {
-    console.log("pull request already there, skip");
-  }
-}
+const maybeOpenPullRequest = require("./lib/pull");
+const updateFlutterWorkspace = require("./lib/upgrade");
 
 async function main() {
   try {
