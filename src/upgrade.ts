ddc3879f53b1465342936a7c8cd7494b25774911
Preet Parekh <preetjdp123@gmail.com>
2020-04-28 18:42:23 +0530

    update YamlFile Logic

--- src/upgrade.ts
+++ src/upgrade.ts
@@ -1,13 +1,66 @@
 import { exec } from "@actions/exec"
 import { ApplicationContext } from "./model/applicationContext";
+import { parseDocument } from "yaml"
+import { readFileSync, writeFileSync } from "fs"
+import { YAMLMap, Scalar, Pair } from "yaml/types";
+import { asyncForEach } from "./utils/asyncForEach"
+import { getLatestPackage } from "./utils/getLatestPackage";
 
 async function updateFlutterWorkspace(context: ApplicationContext) {
+  await exec(`git checkout -b ${context.tempBranch}`);
+  // await exec("flutter", ["pub", "upgrade"], {
+  //   cwd: context.flutterProjectWorkspace
+  // });
+  await updateYamlFile(context)
+  await tryPushChanges(context)
+}
+
+async function updateYamlFile(context: ApplicationContext) {
+  // let pubspecFilePath = `./labrat/pubspec.yaml`
+  let pubspecFilePath = `${context.flutterProjectWorkspace}/pubspec.yaml`
+  try {
+    const pubspecFile = readFileSync(pubspecFilePath, 'utf8')
+    let docYaml = parseDocument(pubspecFile)
+    let dependencyMap: YAMLMap = docYaml.get('dependencies')
+    if (!dependencyMap) { throw "No Dependecies Found" }
+
+    let dependeciesList = dependencyMap.items
+    if (dependeciesList.length == 0) { throw "No Dependecies Present" }
+    await asyncForEach(dependeciesList, async (dependency) => {
+      const dependencyKey: Scalar = dependency.key
+      const dependencyValue: Scalar = dependency.value
+
+      let dependencyName = dependencyKey.value
+      let dependencyVersion = dependencyValue.value?.replace('^', '') ?? undefined
+
+      if (!dependencyName) { return }
+      if (!dependencyVersion) { return }
+
+      if (['flutter'].includes(dependencyName)) {
+        return
+      }
+
+      let latestVersion = await getLatestPackage(dependency)
+      if (dependencyVersion != latestVersion) {
+        dependencyMap.set(dependencyKey, latestVersion.version)
+      }
+      console.log(`
+        Package Name => ${dependencyName}
+        Current Version => ${dependencyVersion}
+        Latest Version => ${latestVersion.version}`)
+    })
+
+    writeFileSync(pubspecFilePath, docYaml.toString())
+
+  } catch (e) {
+    console.log("There is a error")
+    console.log(e)
+  }
+}
+
+async function tryPushChanges(context: ApplicationContext) {
   let infoOutput = "";
   let errorOuput = "";
-  await exec(`git checkout -b ${context.tempBranch}`);
-  await exec("flutter", ["pub", "upgrade"], {
-    cwd: context.flutterProjectWorkspace
-  });
   await exec("git", [
     "config",
     "--global",
@@ -47,4 +100,4 @@ async function updateFlutterWorkspace(context: ApplicationContext) {
   console.log(errorOuput);
 }
 
-export { updateFlutterWorkspace }
+export { updateFlutterWorkspace, updateYamlFile }
